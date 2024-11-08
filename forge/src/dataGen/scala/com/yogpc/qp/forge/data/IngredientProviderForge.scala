778c9da63399992baf559c84d0af7fe4f7d22961
Kotori316 <Kotori316@gmail.com>
2024-11-08 21:41:58 +0900

    Update forge recipes

--- forge/src/dataGen/scala/com/yogpc/qp/forge/data/IngredientProviderForge.scala
+++ forge/src/dataGen/scala/com/yogpc/qp/forge/data/IngredientProviderForge.scala
@@ -1,6 +1,7 @@
 package com.yogpc.qp.forge.data
 
 import com.yogpc.qp.data.IngredientProvider
+import net.minecraft.core.HolderGetter
 import net.minecraft.data.recipes.RecipeOutput
 import net.minecraft.tags.TagKey
 import net.minecraft.world.item.crafting.Ingredient
@@ -8,29 +9,29 @@ import net.minecraft.world.item.{Item, Items}
 import net.minecraftforge.common.Tags
 import net.minecraftforge.common.crafting.conditions.FalseCondition
 
-final class IngredientProviderForge extends IngredientProvider {
+final class IngredientProviderForge(itemRegistry: HolderGetter[Item]) extends IngredientProvider(itemRegistry) {
 
-  override def glowStoneDust: Ingredient = Ingredient.of(Tags.Items.DUSTS_GLOWSTONE)
+  override def glowStoneDust: Ingredient = Ingredient.of(itemRegistry.getOrThrow(Tags.Items.DUSTS_GLOWSTONE))
 
-  override def redStoneDust: Ingredient = Ingredient.of(Tags.Items.DUSTS_REDSTONE)
+  override def redStoneDust: Ingredient = Ingredient.of(itemRegistry.getOrThrow(Tags.Items.DUSTS_REDSTONE))
 
-  override def lapis: Ingredient = Ingredient.of(Tags.Items.GEMS_LAPIS)
+  override def lapis: Ingredient = Ingredient.of(itemRegistry.getOrThrow(Tags.Items.GEMS_LAPIS))
 
-  override def diamond: Ingredient = Ingredient.of(Tags.Items.GEMS_DIAMOND)
+  override def diamond: Ingredient = Ingredient.of(itemRegistry.getOrThrow(Tags.Items.GEMS_DIAMOND))
 
-  override def ironIngot: Ingredient = Ingredient.of(Tags.Items.INGOTS_IRON)
+  override def ironIngot: Ingredient = Ingredient.of(itemRegistry.getOrThrow(Tags.Items.INGOTS_IRON))
 
-  override def goldIngot: Ingredient = Ingredient.of(Tags.Items.INGOTS_GOLD)
+  override def goldIngot: Ingredient = Ingredient.of(itemRegistry.getOrThrow(Tags.Items.INGOTS_GOLD))
 
   override def obsidianTag: TagKey[Item] = Tags.Items.OBSIDIANS
 
-  override def glass: Ingredient = Ingredient.of(Tags.Items.GLASS_BLOCKS)
+  override def glass: Ingredient = Ingredient.of(itemRegistry.getOrThrow(Tags.Items.GLASS_BLOCKS))
 
-  override def redStoneBlock: Ingredient = Ingredient.of(Tags.Items.STORAGE_BLOCKS_REDSTONE)
+  override def redStoneBlock: Ingredient = Ingredient.of(itemRegistry.getOrThrow(Tags.Items.STORAGE_BLOCKS_REDSTONE))
 
-  override def goldBlock: Ingredient = Ingredient.of(Tags.Items.STORAGE_BLOCKS_GOLD)
+  override def goldBlock: Ingredient = Ingredient.of(itemRegistry.getOrThrow(Tags.Items.STORAGE_BLOCKS_GOLD))
 
-  override def diamondBlock: Ingredient = Ingredient.of(Tags.Items.STORAGE_BLOCKS_DIAMOND)
+  override def diamondBlock: Ingredient = Ingredient.of(itemRegistry.getOrThrow(Tags.Items.STORAGE_BLOCKS_DIAMOND))
 
   override def pickaxeForQuarry: Ingredient = Ingredient.of(Items.DIAMOND_PICKAXE)
 
@@ -41,11 +42,11 @@ final class IngredientProviderForge extends IngredientProvider {
     }
   }
 
-  override def enderPearl: Ingredient = Ingredient.of(Tags.Items.ENDER_PEARLS)
+  override def enderPearl: Ingredient = Ingredient.of(itemRegistry.getOrThrow(Tags.Items.ENDER_PEARLS))
 
-  override def amethyst: Ingredient = Ingredient.of(Tags.Items.GEMS_AMETHYST)
+  override def amethyst: Ingredient = Ingredient.of(itemRegistry.getOrThrow(Tags.Items.GEMS_AMETHYST))
 
-  override def netherStar: Ingredient = Ingredient.of(Tags.Items.NETHER_STARS)
+  override def netherStar: Ingredient = Ingredient.of(itemRegistry.getOrThrow(Tags.Items.NETHER_STARS))
 
-  override def emeraldBlock: Ingredient = Ingredient.of(Tags.Items.STORAGE_BLOCKS_EMERALD)
+  override def emeraldBlock: Ingredient = Ingredient.of(itemRegistry.getOrThrow(Tags.Items.STORAGE_BLOCKS_EMERALD))
 }
