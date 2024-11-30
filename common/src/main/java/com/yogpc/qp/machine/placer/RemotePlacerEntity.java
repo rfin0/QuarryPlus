3480b1263e58893005d57bcd7ad823cf7fe3cf37
Kotori316 <Kotori316@gmail.com>
2024-12-01 00:35:35 +0900

    Add test for Remote Placer

--- common/src/main/java/com/yogpc/qp/machine/placer/RemotePlacerEntity.java
+++ common/src/main/java/com/yogpc/qp/machine/placer/RemotePlacerEntity.java
@@ -18,6 +18,7 @@ public final class RemotePlacerEntity extends AbstractPlacerTile {
         targetPos = pos.above();
     }
 
+    @NotNull
     @Override
     protected BlockPos getTargetPos() {
         return targetPos;
